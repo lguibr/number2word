fff7f97b1383f8eb36f0ca8a3a2335de17d3de32
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2024-04-18 20:52:57 -0300

    [FEA] Create page components for result and home pages
    * Add `Form.vue` component for the home page
      - Implement number input field with validation using Zod
      - Handle form submission and navigate to result page
    * Add `View.vue` component for the home page
      - Use `Card` component to wrap the form
      - Center the content vertically and horizontally
    * Utilize `Card` and `Spinner` components from core directory
    
    The new components provide the structure and functionality for the home page, allowing users to enter a number and navigate to the result page upon successful form submission. The form includes input validation to ensure data integrity and user feedback.

---
+++ ui/components/result/View.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" xs="12" sm="8" md="6" lg="6" xl="6">
+        <Card title="Number converted in words:">
+          <CopyDisplay :copyText="numInWords"></CopyDisplay>
+        </Card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script >
+import Card from "./../../components/core/Card.vue"
+import CopyDisplay from "./../../components/result/CopyDisplay.vue"
+export default {
+  components: { Card, CopyDisplay },
+  data() {
+    return {
+      numInWords: '',
+      number: "",
+    };
+  },
+  created() {
+    this.fetchNumToWords();
+  },
+  methods: {
+    notifyError(error) {
+      const { $bus } = useNuxtApp()
+      console.log({ error })
+
+      const defaultError = "Unknown error please enter in contact with the administrator "
+      const message = !!error ? error : defaultError
+      console.log({ message })
+
+      $bus.$emit('notify', { message, type: "error" })
+    },
+    async fetchNumToWords() {
+      const { number } = this.$route.params;
+      this.number = number;
+
+      let numInWords
+      let errorMessage
+
+      await useFetch(`/api/num_to_english/${number}`, {
+        onResponse({ response }) {
+          if (response.ok) {
+            const data = response._data.data
+            const { num_in_english } = data
+            numInWords = num_in_english
+          } else {
+            const error = response._data.error
+            const { message } = error
+            errorMessage = message
+          }
+        },
+      })
+
+      if (numInWords)
+        this.numInWords = numInWords
+      else {
+        this.notifyError(errorMessage)
+        this.$router.push(`/`);
+      }
+    }
+  }
+};
+</script>
