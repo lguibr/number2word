3d1763d9f6306e36f4f25714ac4dedb9ad6ab8d6
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2024-04-19 19:42:01 -0300

    FEA (New feature) Update CI/CD pipeline and deploy UI app to S3 bucket
    * Split CI/CD pipeline into separate jobs for UI and backend
      - Introduce `ui-code-quality` job to run linting on UI code
      - Make `build-ui` job dependent on `ui-code-quality` job
      - Rename `code-quality` job to `backend-code-quality`
      - Make `tests` job dependent on `backend-code-quality` job
    * Add `cdk-ui-diff` job to run CDK diff for UI stack on pull requests
    * Update S3 bucket configuration for UI app deployment
      - Set bucket name to `bucket-trellis-law-ui-adhoc`
      - Configure object ownership, public access, lifecycle rules, and CORS settings
      - Add resource policy to allow public read access to objects in the bucket
    * Output the URL of the deployed UI app
    
    The changes enhance the CI/CD pipeline by separating UI and backend jobs, introducing UI-specific jobs, and deploying the UI app to an S3 bucket with appropriate configurations. This allows for more granular control over the pipeline and enables deploying the UI app independently.

--- infra/lib/ui-stack.ts
+++ infra/lib/ui-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export class UiAppDeploymentStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -8,11 +9,41 @@ export class UiAppDeploymentStack extends cdk.Stack {
 
     // Create a new S3 bucket to hold the UI application
     const bucket = new s3.Bucket(this, "UiAppBucket", {
-      bucketName: `bucket-trellis-law-ui-testing`,
+      bucketName: `bucket-trellis-law-ui-adhoc`,
       websiteIndexDocument: "index.html",
       publicReadAccess: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // Automatically delete bucket when the stack is deleted
+      objectOwnership: s3.ObjectOwnership.OBJECT_WRITER,
+      blockPublicAccess: new s3.BlockPublicAccess({
+        blockPublicAcls: false,
+        ignorePublicAcls: false,
+        blockPublicPolicy: false,
+        restrictPublicBuckets: false,
+      }),
+      lifecycleRules: [
+        {
+          abortIncompleteMultipartUploadAfter: cdk.Duration.days(7),
+        },
+      ],
+      cors: [
+        {
+          allowedOrigins: ["*"],
+          allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.HEAD],
+          allowedHeaders: ["*"],
+          exposedHeaders: [],
+          maxAge: 3000,
+        },
+      ],
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
+
+    const public_policy = new iam.PolicyStatement({
+      actions: ["s3:GetObject"],
+      effect: iam.Effect.ALLOW,
+      principals: [new iam.AnyPrincipal()],
+      resources: [bucket.arnForObjects("*")],
+    });
+
+    bucket.addToResourcePolicy(public_policy);
 
     // Deploy files from the local 'build' directory to the S3 bucket
     new s3deploy.BucketDeployment(this, "DeployUiApp", {
@@ -20,6 +51,7 @@ export class UiAppDeploymentStack extends cdk.Stack {
       destinationBucket: bucket,
     });
 
+    // Output the URL of the website
     new cdk.CfnOutput(this, "SiteURL", {
       value: bucket.bucketWebsiteUrl,
       description: "The URL of the website",
@@ -31,6 +63,6 @@ const app = new cdk.App();
 new UiAppDeploymentStack(app, "UiAppDeploymentStack", {
   env: {
     region: process.env.AWS_REGION,
-    account: process.env.AWS_UI_ACCOUNT,
+    account: process.env.AWS_ACCOUNT,
   },
 });
