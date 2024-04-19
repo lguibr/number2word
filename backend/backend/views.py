a0b145272f7792ebe5528beb1f958a6b82b5a0b7
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2024-04-19 19:45:19 -0300

    [FEA] Add type annotations and improve import structure in views.py
    * Add type annotations for function parameters and return values
      - `swagger_view` takes a `Request` object and returns a `JsonResponse`
      - `swagger_ui_view` takes a `Request` object and returns an `HttpResponse`
    
    * Update import statements for better readability and organization
      - Group imports from Django together
      - Add a separate import for `rest_framework.request.Request`
    
    The changes enhance code clarity, maintainability, and align with best practices by leveraging type annotations and improving the import structure.

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -3,16 +3,17 @@ import json
 import os
 
 from django.conf import settings
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from rest_framework.request import Request
 
 
-def swagger_view(request):
+def swagger_view(request: Request) -> JsonResponse:
     path = os.path.join(settings.BASE_DIR, "./static/swagger.json")
     with open(path, "r") as file:
         data = json.load(file)
     return JsonResponse(data)
 
 
-def swagger_ui_view(request):
+def swagger_ui_view(request: Request) -> HttpResponse:
     return render(request, "./swagger_ui.html")
